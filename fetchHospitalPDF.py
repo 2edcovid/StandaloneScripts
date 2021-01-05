a857e4cc0973c4a569b83eea0a3b2bbfc42b9e5b
2eD <2edCovid@gmail.com>
2021-01-05 10:34:40 -0600

    CSV Format with Date and Totals

--- fetchHospitalPDF.py
+++ fetchHospitalPDF.py
@@ -3,6 +3,7 @@ import time
 import fileNames
 import csv
 import json
+import re
 
 from seleniumUtils import *
 import urls
@@ -66,19 +67,68 @@ def readPDF(pdfFile):
         print('found county data')
         break
 
+    leftoverStrings = []
+    readingCountyData = True
     for stringData in viewer.canvas.strings:
-      if not stringData.isnumeric():
-        compiled = compiled + stringData
+      if readingCountyData:
+        if not stringData.isnumeric():
+          compiled = compiled + stringData
+        else:
+          countyHospitalData[compiled] = stringData
+          if compiled == 'Wright':
+            readingCountyData = False
+          compiled = ""
       else:
-        countyHospitalData[compiled] = stringData
-        if compiled == 'Wright':
-          break
-        compiled = ""
+        leftoverStrings.append(stringData)
+
+    parsedLeftOvers = ''
+    leftoverVals = []
+    for string in leftoverStrings:
+      if not string.isnumeric():
+        parsedLeftOvers = parsedLeftOvers + string
+      elif string == '19' and parsedLeftOvers.lower().endswith('covid-'):
+        parsedLeftOvers = parsedLeftOvers + string
+      elif parsedLeftOvers.endswith('/'):
+        parsedLeftOvers = parsedLeftOvers + string
+      else:
+        leftoverVals.append(string)
+
+    parsedLeftOvers = parsedLeftOvers.lower()
+    parsedLeftOvers = parsedLeftOvers.replace('covid-19', '')
+    parsedLeftOvers = parsedLeftOvers.replace('county', '')
+    parsedLeftOvers = parsedLeftOvers.replace('patients', '')
+    parsedLeftOvers = parsedLeftOvers.replace('confirmed', '')
+
+    dateRegex = r'.+(\d+\/\d+\/\d+)'
+    matches = re.match(dateRegex, parsedLeftOvers)
+
+    date = "couldn't read date"
+    if matches:
+      date = matches.group(1)
+
+    countyHospitalData['Out Of State'] = leftoverVals[0]
+    countyHospitalData['Total Iowans'] = leftoverVals[1]
+    countyHospitalData['Total Hospitalized'] = leftoverVals[2]
+    countyHospitalData['Date'] = date
+
   except Exception as e:
     print('no hospital data {}'.format(e))
 
   return countyHospitalData
 
+def writeHospitalCSV(filePath, data):
+  csvHeaders = ["County", "Hospitalized"]
+  rows = []
+  
+  for key in data:
+    rows.append( {"County" : key,
+                  "Hospitalized" : data[key]}
+              )
+
+  with open(filePath, 'w',encoding='utf-8',newline='',) as f:
+    writer = csv.DictWriter(f, csvHeaders)
+    writer.writeheader()
+    writer.writerows(rows)
 
 def writeJson(filePath, data):
   if os.path.exists(filePath):
@@ -89,5 +139,7 @@ def writeJson(filePath, data):
 
 if __name__ == "__main__":
   timeString = time.strftime("%Y-%m-%d %H%M")
-  hospitalPDFData = readPDF(getHospitalData(timeString))
-  writeJson('hospitalData-{}.csv'.format(timeString), hospitalPDFData)
+  pdfFile = getHospitalData(timeString)
+  hospitalPDFData = readPDF(pdfFile)
+  # writeJson('hospitalData-{}.csv'.format(timeString), hospitalPDFData)
+  writeHospitalCSV('hospitalData-{}.csv'.format(timeString), hospitalPDFData)
